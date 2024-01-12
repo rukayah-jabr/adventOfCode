120308e5d84e54abef958f0aaf82fc17d16c41af
rukayah-jabr <rukayah.jabr@stud.fh-campuswien.ac.at>
2024-01-12 19:58:19 +0100

    added part 2 to the solution

--- src/Boat.java
+++ src/Boat.java
@@ -1,10 +1,10 @@
 public class Boat {
 
-    public int calculatePossibleWays(int duration, int recordDistance) {
-        int possibilities = 0;
-        for (int holdButton = 0; holdButton < duration; holdButton++) {
-            int remainingTime = duration - holdButton - 1;
-            int distance = (holdButton + 1) * remainingTime;
+    public long calculatePossibleWays(long duration, long recordDistance) {
+        long possibilities = 0;
+        for (long holdButton = 0; holdButton < duration; holdButton++) {
+            long remainingTime = duration - holdButton - 1;
+            long distance = (holdButton + 1) * remainingTime;
 
             if (distance > recordDistance) {
                 possibilities++;
--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,28 @@ public class Main{
     public static void main(String[] args) {
         Boat boat = new Boat();
 
-        int[] durations = {46, 82, 84, 79};
-        int[] distances = {347, 1522, 1406, 1471};
-        int sumOfPossibilities = 1;
 
+        //Teil 1
+        int[] durations = {46,82,84,79};
+        long[] distances = {347L,1522L,1406L,1471L};
+        long sumOfPossibilities = 1;
+
+        //Teil 2
+        long duration = 46828479;
+        long distance = 347152214061471L;
+
+        //Teil 1
         for (int i = 0; i < durations.length; i++) {
-            int ways = boat.calculatePossibleWays(durations[i], distances[i]);
+            long ways = boat.calculatePossibleWays(durations[i], distances[i]);
             sumOfPossibilities *= ways;
         }
 
         System.out.println(sumOfPossibilities);
+
+        //Teil2
+        sumOfPossibilities = boat.calculatePossibleWays(duration, distance);
+        System.out.println(sumOfPossibilities);
+
+
     }
 }
